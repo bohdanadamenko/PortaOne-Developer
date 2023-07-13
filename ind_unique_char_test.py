c859a251e04d6a0182a1bc7de5fe85cc09468b1c
Bohdan <89193347+bohdanadamenko@users.noreply.github.com>
2023-07-13 14:47:50 +0200

    Create ind_unique_char_test.py

---
+++ ind_unique_char_test.py
@@ -0,0 +1,4 @@
+from find_unique_char import UniqueLetterFinder
+finder = UniqueLetterFinder(
+    "The Tao gave birth to machine language. Machine language gave birth to the assembler.")
+print(finder.find_first_unique_letter()) # Return 'm'
